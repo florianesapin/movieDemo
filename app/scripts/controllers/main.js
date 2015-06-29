6fbdaaaa56453f89bc8d05bb343dc842e6abc0a4
florianesapin <floriane.sapin@IG14-SapinF.intra.eiaj.ch>
2015-06-29 19:37:08 +0200

    suppression de film dans la liste

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,18 +10,20 @@
 angular.module('movieDemoApp')
   .controller('MainCtrl', function ($scope) {
     $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
     ];
-    for(var i =0; i< 100; i++){
+    for(var i =0; i< 0; i++){
 		$scope.awesomeThings.push('Element ' + (i + 1));
     }
-	$scope.nbrAffiche = 20;
+	$scope.nbrAffiche = 5;
 	$scope.active = true;
 	$scope.monClick = function(){
 		$scope.awesomeThings.push($scope.nouveauFilm);
 		$scope.nouveauFilm = '';
 	}
+
+	$scope.remove = function(el){
+  		var index = $scope.awesomeThings.indexOf(el);
+  		$scope.awesomeThings.splice(index, 1); 
+	};
   });
 
